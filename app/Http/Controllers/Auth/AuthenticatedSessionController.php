b038307090ad9fa7ebb590142e2bde0f122177ad
Edi Kurniawan <edikurniawan.dev@gmail.com>
2025-04-21 10:36:35 +0700

    refactor: :recycle: auth

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Response;
 
@@ -36,7 +35,7 @@ class AuthenticatedSessionController extends Controller {
    * Destroy an authenticated session.
    */
   public function destroy(Request $request): RedirectResponse {
-    Auth::guard("web")->logout();
+    auth()->guard("web")->logout();
 
     $request->session()->invalidate();
     $request->session()->regenerateToken();
