d12ff0ab885af1a76676962cc84dc3008fd54bf4
Edi Kurniawan <edikurniawan.dev@gmail.com>
2025-04-21 10:36:59 +0700

    feat: :sparkles: add logout button

--- resources/js/pages/auth/confirm-password.tsx
+++ resources/js/pages/auth/confirm-password.tsx
@@ -2,7 +2,7 @@ import AuthLayout from "@/layouts/auth-layout";
 import { __, handleFormErrorMessages } from "@/lib/utils";
 import { SharedData } from "@/types";
 import { router, usePage } from "@inertiajs/react";
-import { App, Button, Form, Input } from "antd";
+import { App, Button, Flex, Form, Input } from "antd";
 import { useState } from "react";
 
 type ConfirmPasswordFormType = {
@@ -11,7 +11,7 @@ type ConfirmPasswordFormType = {
 
 export default function ConfirmPassword() {
   const { locale } = usePage<SharedData>().props;
-  const { message } = App.useApp();
+  const { modal, message } = App.useApp();
 
   const [isProcessing, setIsProcessing] = useState(false);
   const [formConfirmPassword] = Form.useForm();
@@ -26,6 +26,37 @@ export default function ConfirmPassword() {
       },
       onFinish: () => {
         setIsProcessing(false);
+      },
+    });
+  };
+
+  const onLogout = () => {
+    const modalConfirm = modal.confirm({
+      title: __(locale, "modal_confirm.title"),
+      content: __(locale, "modal_confirm.desc"),
+      okText: __(locale, "auth.logout"),
+      okButtonProps: { danger: true },
+      cancelButtonProps: { disabled: false },
+      onOk: () => {
+        modalConfirm.update({
+          cancelButtonProps: { disabled: true },
+        });
+
+        return new Promise((resolve) => {
+          router.post(
+            route("logout"),
+            {},
+            {
+              onSuccess: () => {
+                message.success(__(locale, "message.success"));
+              },
+              onError: () => {
+                message.error(__(locale, "message.error_server"));
+              },
+              onFinish: resolve,
+            }
+          );
+        });
       },
     });
   };
@@ -53,6 +84,12 @@ export default function ConfirmPassword() {
         <Button block type="primary" htmlType="submit" loading={isProcessing}>
           {__(locale, "auth.confirm_password")}
         </Button>
+
+        <Flex justify="center">
+          <Button type="link" style={{ marginBlock: 8 }} onClick={onLogout}>
+            {__(locale, "auth.logout")}
+          </Button>
+        </Flex>
       </Form>
     </AuthLayout>
   );
