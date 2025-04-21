0b764725a5c5d287fa73350b67cba927be052fc5
Edi Kurniawan <edikurniawan.dev@gmail.com>
2025-04-21 15:14:43 +0700

    refactor: :fire: change path

--- config/telescope.php
+++ config/telescope.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-  "path" => env("TELESCOPE_PATH", "telescope"),
+  "path" => env("TELESCOPE_PATH", "dashboard/laravel-telescope"),
 
   /*
     |--------------------------------------------------------------------------
